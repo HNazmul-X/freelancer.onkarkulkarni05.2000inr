49b358720c502c05eb3b5fa9030b2e8910c56aa0
H. Nazmul Hassan <nazmul.w3@gmail.com>
2021-08-11 18:09:48 +0600

    url redirection added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import DefaultRouter from "./Routes/defaultRouter";
-import Navbar from "./Components/Navbar/Navbar.jsx"
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar/>
-      <DefaultRouter/>
-    </BrowserRouter>
-  );
+    try {
+        if (document.location.pathname === "/" && document.location.hash === "") {
+            document.location.href = document.location.href + "#header";
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <DefaultRouter />
+        </BrowserRouter>
+    );
 }
 
 export default App;
